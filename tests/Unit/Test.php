97b92a00067e6255c614b5010a4be847264c7762
Richard Joseph Porter <rjosephporter@gmail.com>
2021-08-07 16:21:55 +0800

    added example unit test

---
+++ tests/Unit/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use ExampleTest;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function exampleTest()
+    {
+        $this->assertTrue(true);
+    }
+}
